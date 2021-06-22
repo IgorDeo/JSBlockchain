67c3eb08e39c376ffdf43d64c834d4111894ec3b
Igor Deo <igor.deo@investtools.com.br>
2021-06-21 22:10:19 -0300

    Manipulacao do DOM na index.html

---
+++ src/scripts/_blockDiv.js
@@ -0,0 +1,33 @@
+
+const blockDiv = (block) => {
+     return(
+                `<div>
+                    <label>Index:</label>
+                    <input class="index-input-block"type="text" disabled value="${block.index}">
+                </div>
+
+                <div>
+                    <label>Data: </label>
+                    <input class="data-input-block" type="text"  value="${block.data.value}">
+                </div>
+
+                <div>
+                    <label>Previous Hash: </label>
+                    <input class="phash-input-block" type="text" disabled value="${block.previousHash}">
+                </div>
+
+                <div>
+                    <label>Hash: </label>
+                    <input class="hash-input-block" type="text" disabled value="${block.hash}">
+                </div>
+
+                <div>
+                    <label>Nonce:</label>
+                    <input class="nonce-input-block" type="text" disabled value="${block.nonce}">
+                </div>`
+        );
+    }
+
+
+
+module.exports = blockDiv;
